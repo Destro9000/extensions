747a73c8ba3ae7fcbe79a19ac3c1e2409ac1a98f
Spektre <77621360+Destro9000@users.noreply.github.com>
2024-03-13 13:15:58 +0100

    ..

--- src/en/Novelhall.lua
+++ src/en/Novelhall.lua
@@ -94,8 +94,7 @@ local searchFilters = {
 --- @return string @of chapter
 local function getPassage(chapterURL)
     local htmlElement = GETDocument(expandURL(chapterURL))
-    local title = htmlElement:selectFirst("h1"):text()
-    local ht = "<h1>" .. title .. "</h1>"
+    local ht = htmlElement:
     local pTagList = map(htmlElement:select(".content-inner br"), text)
     for k,v in pairs(pTagList) do ht = ht .. "<p><p>" .. v end
     return pageOfElem(ht, true)
