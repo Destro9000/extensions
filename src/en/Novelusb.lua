f33794dfc33db9f51f84f665aadcbbbca59200df
Destro9000 <sehailabc@gmail.com>
2025-04-05 15:22:56 +0200

    idk

--- src/en/Novelusb.lua
+++ src/en/Novelusb.lua
@@ -1,4 +1,4 @@
--- {"id":95564,"ver":"1.0.10","libVer":"1.0.0","author":"Confident-hate"}
+-- {"id":95564,"ver":"1.0.11","libVer":"1.0.0","author":"Confident-hate"}
 
 local baseURL = "https://novelbin.com"
 
